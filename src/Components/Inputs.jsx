755c890e7b18970ed34b67723160e5bbf6296979
kaur-rajdeep <kaurd3535@gmail.com>
2023-06-14 21:23:09 +0530

    added the meme images data

--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-
+import memesData from "../memesData";
 export default function Inputs(){
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random()*memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return(
         <div className="texts">
         <div className="inputs">
@@ -8,7 +16,7 @@ export default function Inputs(){
         <input type="text" placeholder="second text" id="second"/><br/>
         </div>
         <div className="meme">
-        <button id="meme">Generate Meme</button>
+        <button id="meme" onClick={getMemeImage}>Generate Meme</button>
         </div>
         </div>
     )
