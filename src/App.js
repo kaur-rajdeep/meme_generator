11f377367f215951bc195c6a0d289fd29f95e995
kaur-rajdeep <kaurd3535@gmail.com>
2023-06-15 23:52:07 +0530

    added the image component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
 import Inputs from "./Components/Inputs";
+import Image from "./Components/Image";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <Header/>
       <Inputs/>
+      <Image/>
     </div>
   );
 }
---
+++ src/Components/Image.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Image(){
+    return(
+        <div>
+            <img src=""/>
+        </div>
+    )
+}
+
+export default Image;
