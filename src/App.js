a61764c191e10bd041e1976ca4ce5c78a8972933
kaur-rajdeep <kaurd3535@gmail.com>
2023-06-16 21:47:29 +0530

    added the images to using the useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
-import Inputs from "./Components/Inputs";
-import Image from "./Components/Image";
+import Memes from "./Components/Meme";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Inputs/>
-      <Image/>
+      <Memes/>
     </div>
   );
 }
--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import memesData from "../memesData";
-export default function Inputs(){
 
+export default function Memes(){
+
+    const [memeImage, setMemeImage]=React.useState("")
+    
     function getMemeImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random()*memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return(
+        <main>
         <div className="texts">
         <div className="inputs">
         <input type="text" placeholder="first text" id="first"/>
@@ -19,5 +22,9 @@ export default function Inputs(){
         <button id="meme" onClick={getMemeImage}>Generate Meme</button>
         </div>
         </div>
+        <div id="Image">
+            <img src={memeImage} className="memeImage"/>
+        </div>
+        </main>
     )
 }
