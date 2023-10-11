87ab3ee37407be5460350ce1ef3ebbf22286f1af
kaur-rajdeep <kaurd3535@gmail.com>
2023-10-11 10:53:46 +0530

    added useState to keep track of text and images

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -2,29 +2,45 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Memes(){
+  // const [memeImage, setMemeImage] = React.useState(
+  //   "http://i.imgflip.com/1bij.jpg"
+  // );
 
-    const [memeImage, setMemeImage]=React.useState("")
-    
-    function getMemeImage(){
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random()*memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-    }
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    return(
-        <main>
-        <div className="texts">
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  return (
+    <main>
+      <div className="texts">
         <div className="inputs">
-        <input type="text" placeholder="first text" id="first"/>
-        <input type="text" placeholder="second text" id="second"/><br/>
+          <input type="text" placeholder="first text" id="first" />
+          <input type="text" placeholder="second text" id="second" />
+          <br />
         </div>
         <div className="meme">
-        <button id="meme" onClick={getMemeImage}>Generate Meme</button>
+          <button id="meme" onClick={getMemeImage}>
+            Generate Meme
+          </button>
         </div>
-        </div>
-        <div id="Image">
-            <img src={memeImage} className="memeImage"/>
-        </div>
-        </main>
-    )
+      </div>
+      <div id="Image">
+        <img src={meme.randomImage} className="memeImage" />
+      </div>
+    </main>
+  );
 }
